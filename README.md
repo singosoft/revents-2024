5071c8b119f20ce34f5a2faff60c3a8db5160fb4
singosoft <singosoft80@gmail.com>
2024-01-27 15:58:41 +0700

    End of section 1

---
+++ README.md
@@ -0,0 +1,2 @@
+# Revents Udemy Course
+
---
+++ src/app/layout/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <h1>
+    Welcome to Revents
+   </h1>
+  )
+}
+
+export default App
